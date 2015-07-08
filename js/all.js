eef783543e1daf416b970697d3438e783b164284
hungjie19 <hungjie19@gmail.com>
2015-07-08 22:22:48 +0800

    blood data done

--- js/all.js
+++ js/all.js
@@ -1,6 +1,13 @@
 angular
 .module('app', [])
 .controller('MainController',function ($log, $http, $scope) {
-    $scope.type = 'people';
-   
+    $scope.type = 'blood';
+   	$http.get('json/blood.json')
+   	.success(function(data) {
+      	$scope.bloodList = data;
+      	console.log('success'+ data);
+    })
+    .error(function (data) {
+    	console.log('error' + data);
+    });
 });
