0f7ed552971277b4fdbcba4eea865603e86478ed
hungjie19 <hungjie19@gmail.com>
2015-07-10 00:15:30 +0800

    add blood status, blood icon

--- js/all.js
+++ js/all.js
@@ -2,6 +2,7 @@ angular
 .module('app', [])
 .controller('MainController',function ($log, $http, $scope) {
     $scope.type = 'blood';
+
    	$http.get('json/blood.json')
    	.success(function(data) {
       	$scope.bloodList = data;
@@ -10,4 +11,33 @@ angular
     .error(function (data) {
     	console.log('error' + data);
     });
+
+    // blood Status
+    $http.get('http://g0v.github.io/blood/blood.json')
+   	.success(function(data) {
+      	$scope.bloodStatus = data;
+      	console.log('success bloodStatus'+ data);
+    })
+    .error(function (data) {
+    	console.log('error' + data);
+    });
+
+    
+});
+
+.filter('bloodString', function(){
+	return function(str){
+		if(str == full){
+			return '正常';
+		}
+		else if(str == medium){
+			return '偏低';	
+		}
+		else if(str == empty){
+			return '極缺';	
+		}
+
+		
+	};
+		}
 });
