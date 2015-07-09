e12bdde8603d422a6dd5fe910f0422d039df521d
bbandydd <bbandydd@gmail.com>
2015-07-10 00:03:56 +0800

    remove services

--- js/services/people.js
+++
@@ -1,9 +0,0 @@
-app.factory('people', ['$http', function people($http){
-    return $http.get('http:////tonyq.org/kptaipei/api-20150628.php')
-        .success(function(data){
-            return data;
-        })
-        .error(function(err){
-            return err;
-        });
-}]);
