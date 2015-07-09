650a41aac731c0eddee3132da0dd234059ef0682
bbandydd <bbandydd@gmail.com>
2015-07-09 23:53:10 +0800

    家屬尋人完成

--- js/all.js
+++ js/all.js
@@ -1,6 +1,4 @@
-angular
-.module('app', [])
-.controller('MainController',function ($log, $http, $scope) {
+app.controller('MainController',function ($log, $http, $scope) {
     $scope.type = 'blood';
    	$http.get('json/blood.json')
    	.success(function(data) {
@@ -10,4 +8,12 @@ angular
     .error(function (data) {
     	console.log('error' + data);
     });
+
+    $http.get('http:////tonyq.org/kptaipei/api-20150628.php')
+        .success(function(data){
+            $scope.peopleList = data;
+        })
+        .error(function(err){
+            console.log('error' + data);
+        });
 });
